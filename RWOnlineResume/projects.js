671226cdb20f396f898c32f986ef59e202142d06
RedFox95 <ryanweidling@gmail.com>
2023-02-28 19:34:17 -0500

    Updated projects to be toggleable and experience

--- RWOnlineResume/projects.js
+++ RWOnlineResume/projects.js
@@ -1,1 +1,14 @@
-const dropdownbutton = document.getElementsByClassName('dropbtn');
+document.addEventListener("DOMContentLoaded", function() {
+    // get all the buttons and their corresponding content divs
+    const buttons = document.querySelectorAll('.btn-group button');
+    const contentDivs = document.querySelectorAll('.btn-group div');
+
+    // add a click event listener to each button
+    buttons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+        // toggle the display of the corresponding content div
+        contentDivs[index].classList.toggle('hidden');
+    });
+    });
+
+  });
