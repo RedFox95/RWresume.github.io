81e92def3ed9ac40bfc5450fdc758a9b3735493b
Ryan Weidling <ryanweidling@gmail.com>
2024-05-13 15:40:12 -0400

    redoing website initial changes incoming

---
+++ RWOnlineResume/projects.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // get all the buttons and their corresponding content divs
+    const buttons = document.querySelectorAll('.btn-group button');
+    const contentDivs = document.querySelectorAll('.btn-group div');
+
+    // add a click event listener to each button
+    buttons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+        // toggle the display of the corresponding content div
+        contentDivs[index].classList.toggle('hidden');
+    });
+    });
+
+  });
+
+  let buttons = document.getElementsByClassName('btn-group');
+  buttons.addEventListener('')
+
+  let hide = function() {
+    
+  }
